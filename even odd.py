8452bda56ccf45877a095cb10f05598dd00d2c86
SUJATRA BISWAS <135025232+PyCodeWizard-SujatraBiswas@users.noreply.github.com>
2023-06-05 11:04:07 +0530

    IIT M PRAVARTAK ASSIGNMENT
    ADVANCE LEVEL ASSIGNMENT

---
+++ even odd.py
@@ -0,0 +1,7 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+even = [x for x in numbers if x%2 == 0]
+odd = [x for x in numbers if x%2 != 0]
+def my_function(x):
+  print(even)
+  print(odd)
+my_function(numbers)
